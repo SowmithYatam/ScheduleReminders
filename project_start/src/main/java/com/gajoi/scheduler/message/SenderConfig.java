738369797247a1277bbf339fce2b964b437a2ea6
SowmithYatam <75824424+SowmithYatam@users.noreply.github.com>
2022-07-10 22:34:33 +0530

    Update SenderConfig.java

--- project_start/src/main/java/com/gajoi/scheduler/message/SenderConfig.java
+++ project_start/src/main/java/com/gajoi/scheduler/message/SenderConfig.java
@@ -6,8 +6,8 @@ import com.twilio.type.PhoneNumber;
 
 public class SenderConfig {
 
-    public static final String ACCOUNT_SID = "AC49501e99525ca4614abe0ae479e57ae8";
-    public static final String AUTH_TOKEN = "2e88ca43fab999840a474e6b2a794e5b";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
     public static final String currNumber = "+15005550006";
 
 
@@ -20,7 +20,7 @@ public class SenderConfig {
         builder.append(title);
         builder.append(System.lineSeparator());
         builder.append(message);
-        Message msg = Message.creator(new PhoneNumber(phoneNumber), "MG45c7a50755afa5aa2c11f65c30bac8bc", builder.toString()).create();
+        Message msg = Message.creator(new PhoneNumber(phoneNumber), "", builder.toString()).create();
         msg.getSid();
     }
 }
